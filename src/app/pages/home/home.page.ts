ac0f34e64820fef7ebaf3512a51cfb6d64623873
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-12-14 04:49:22 -0300

    ...

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,18 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  
 
   constructor(
     private router: Router,
-    private menuCtrl: MenuController
+    private menuCtrl: MenuController,
+    
   ) {}
-
+    
     ngOnInit() {
   }
   ionViewWillEnter() {
